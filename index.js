08e156b4f03f2ae28688da0bf912302b39c9d406
wataruoguchi <wataru.oguchi.0to1@gmail.com>
2015-01-21 11:32:44 -0800

    Add Ajax version
    called flickr2. access via root folder, click flickrCMS2.html

--- index.js
+++ index.js
@@ -8,37 +8,10 @@ server.connection({
     port: 8000
 });
 
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello', 
-    handler: function (request, reply) {
-       reply('hello world');
-    }
-});
-
 server.views({
 	engines: {html:require('hapi-dust')},
 	path: __dirname + "/dust_templates"
 });
-
-/*server.route({
-    method: 'GET',
-    path:'/flickr', 
-    handler: function (request, reply) {
-		//reference: https://www.npmjs.com/package/request
-		var request = require('request');
-		request('http://www.google.com', function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				//console.log(body); // Print the google web page. You can see it in command prompt
-				reply(body);
-			}
-		})
-
-		//reply('Goal is return Flickr JSON');
-		//TODO: inclass currently the google home page HTML is outputted in the command window. Change this to output in the browser window!
-    }
-});*/
 
 /** Conditions
 * As a user, I want to see photos tagged with Vancouver. (bonus)
@@ -48,10 +21,9 @@ server.views({
  (3)photos tagged with Vancouver
  (4)Bonus will have jQuery AJAX consume the light response and display HTML images. (I don't take this since I use template)
 */
-
 server.route({
     method: 'GET',
-    path:'/flickr', 
+    path:'/flickr',
     handler: function (request, reply) {
     	//get credentials
     	var credentials = require('./credentials.js'),
@@ -97,3 +69,69 @@ server.route({
 server.start(function(){
 	console.log("Server running at "+server.info.uri);
 });
+
+// Different approach on Jan. 21th ---- start
+server.route ({
+	method: 'GET',
+	path: '/{param*}',
+	handler: {
+		directory: {
+			path: "public",
+			listing: true,
+			index: false
+		}
+	}
+});
+server.route ({
+	method: 'GET',
+	path: '/shared/{param*}',
+	handler: {
+		directory: {
+			path: "shared",
+			listing: true,
+			index: false
+		}
+	}
+});
+
+server.route({
+    method: 'GET',
+    path:'/flickr2',
+    handler: function (request, reply) {
+    	//get credentials
+    	var credentials = require('./shared/credentials.js'),
+		flickrQueryString = {
+    	    		"method": 'flickr.photos.search',
+    	    		"api_key": credentials.flickr.api_key,
+    	    		"tags": 'vancouver',	//(3)photos tagged with Vancouver
+    	    		"format": 'json',
+    	    		"nojsoncallback": 1
+    	    	},
+		httpRequest = require('request');
+
+    	//request to flickr
+		httpRequest({
+			method: 'GET',
+			url: 'https://api.flickr.com/services/rest/',
+			qs: flickrQueryString,
+			json: "true"
+		}, 
+
+    	//get response by frickr
+		function (error, incomingMessage, response) {
+			if (!error && incomingMessage.statusCode == 200) {
+				var photos = response.photos.photo,
+				uriArray = [],
+				uri;
+
+				photos.forEach(function(photo, idx){
+					//(2)includes the fully resolved JPG hyperlinks of photos
+					uri = "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+".jpg";
+					uriArray.push({"uri": uri});					
+				});
+				reply({"photos" : uriArray});
+			}
+		})
+    }
+});
+// Different approach on Jan. 21th ---- end
